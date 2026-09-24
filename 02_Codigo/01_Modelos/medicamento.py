90b1fa371ef5eb1c8e42ba9cb4b8e8c1c53a2a45
fernan127 <fernandovalerin796@gmail.com>
2026-09-24 12:37:51 -0500

    Add Medicamento class with description methods

--- 02_Codigo/01_Modelos/medicamento.py
+++ 02_Codigo/01_Modelos/medicamento.py
@@ -1,1 +1,11 @@
+class Medicamento:
+    def __init__(self, nombre, dosis, frecuencia):
+        self.nombre = nombre
+        self.dosis = dosis
+        self.frecuencia = frecuencia
 
+    def describir(self):
+        return f"{self.nombre} - {self.dosis}, cada {self.frecuencia}"
+
+    def __str__(self):
+        return self.describir()
