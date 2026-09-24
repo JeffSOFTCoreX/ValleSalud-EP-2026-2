5926a13904a45b2b4707a6888e91e0f93c69026f
fernan127 <fernandovalerin796@gmail.com>
2026-09-24 12:54:17 -0500

    Add unit tests for appointment and medication registration

--- 02_Codigo/02_Funciones/pruebas.py
+++ 02_Codigo/02_Funciones/pruebas.py
@@ -1,1 +1,42 @@
+import unittest
 
+from funciones import registrar_cita, registrar_medicamento
+from paciente import Paciente
+
+
+class PruebasPI(unittest.TestCase):
+    def test_registro_de_cita(self):
+        paciente = Paciente("Ana García", "12345678", "3001234567")
+        cita = registrar_cita(
+            paciente,
+            "25/09/2026",
+            "09:00",
+            "Control general",
+        )
+
+        self.assertEqual(len(paciente.citas), 1)
+        self.assertIs(paciente.citas[0], cita)
+        self.assertIs(cita.paciente, paciente)
+
+    def test_registro_de_medicamento(self):
+        paciente = Paciente("Ana García", "12345678", "3001234567")
+        cita = registrar_cita(
+            paciente,
+            "25/09/2026",
+            "09:00",
+            "Control general",
+        )
+        medicamento = registrar_medicamento(
+            cita,
+            "Paracetamol",
+            "500 mg",
+            "8 horas",
+        )
+
+        self.assertEqual(len(cita.medicamentos), 1)
+        self.assertIs(cita.medicamentos[0], medicamento)
+        self.assertEqual(medicamento.nombre, "Paracetamol")
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
